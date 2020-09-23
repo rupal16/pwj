91a790e18e685db56fe50f16808c1ad8917c8934
Rupal Jain <rupaljain.1699@gmail.com>
2020-09-23 23:22:59 +0530

    text updates in the 'Formatted text' section as user types in textarea

--- TextEditorExercise/script.js
+++ TextEditorExercise/script.js
@@ -1,17 +1,18 @@
 /**
- * TODO: Update the text in the "Formatted Text" section as a user types in the textarea
+ * DONE: Update the text in the "Formatted Text" section as a user types in the textarea
  * TODO TOGETHER: Add a .bold, .italic classes to "Formatted Text" when the appropriate button is clicked
  * TODO: Add an .underline class to "Formatted Text" when Underline button is clicked
  * TODO: Toggle the align style for "Formatted Text" when the appropriate button is clicked
  */
 
-
 /**
  * Update the output text as a user types in the textarea
  * HINT: Use the onkeydown function inside HTML
  */
-function updateText(){
+function updateText() {
   // CODE GOES HERE
+  let text = document.getElementById("text-input").value;
+  document.getElementById("text-output").innerText = text;
 }
 
 /**
@@ -21,15 +22,14 @@ function updateText(){
  * HINT: Use the classList property
  * HINT: Toggle .active class for the button
  */
-function makeBold(elem){
+function makeBold(elem) {
   //CODE GOES HERE
 }
 
 /**
  * Toggle the italic class for the output text
  */
-function makeItalic(elem){
-}
+function makeItalic(elem) {}
 
 /**
  * Toggle the underline class for the output text
@@ -37,8 +37,8 @@ function makeItalic(elem){
  * HINT: Use the classList property
  * HINT: Use contains, remove, and add functions
  */
-function makeUnderline(elem){
-    //CODE GOES HERE
+function makeUnderline(elem) {
+  //CODE GOES HERE
 }
 
 /**
@@ -47,6 +47,6 @@ function makeUnderline(elem){
  * HINT: Use the style property of the element
  * HINT: Make sure to untoggle the active state for all other align buttons
  */
-function alignText(elem, alignType){
+function alignText(elem, alignType) {
   // CODE GOES HERE
 }
