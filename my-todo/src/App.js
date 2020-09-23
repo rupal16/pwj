57e44c74463830cbc172c2e66a20c992c8a29600
Rupal Jain <rupaljain.1699@gmail.com>
2020-09-23 12:37:44 +0530

    created the basic structure of the app

--- my-todo/src/App.js
+++ my-todo/src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addATodo = () => {
+    console.log("todo added");
+
+    setTodos([...todos, inputText]);
+  };
+
+  const onChangeHandler = (event) => {
+    setInputText(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Todo app</h1>
+      <input onChange={onChangeHandler} value={inputText} type="text" />
+      <button onClick={addATodo}>ADD A TODO</button>
     </div>
   );
 }
