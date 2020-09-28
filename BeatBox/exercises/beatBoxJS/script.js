d719fc889113f8c9aba1d424f37d83a9b45929a3
Rupal Jain <rupaljain.1699@gmail.com>
2020-09-28 23:25:40 +0530

    completed triggerBeat() to play upon the press of respective keys

--- BeatBox/exercises/beatBoxJS/script.js
+++ BeatBox/exercises/beatBoxJS/script.js
@@ -1,7 +1,7 @@
 /**
  * DONE: Create a keydown listener to track what keys are hit
  * DONE: Create a Beat class to represent the beat object in utils.js
- * TODO: Complete triggerBeat() to play upon the press of a,s,d,f,g,h,j,k,l
+ * DONE: Complete triggerBeat() to play upon the press of a,s,d,f,g,h,j,k,l
  * TODO: Button pt1: Initialize color and element values
  * TODO: Button pt2: Set button color upon initialization | Initialize button in beats["65"]
  * TODO: Button pt3: Complete select function to set the color and shadow of button upon pressing
@@ -49,13 +49,18 @@ let beats = {
  * Function to play the beat upon a press of key
  * HINT: use the keyCode
  */
-triggerBeat = (event) => {};
+triggerBeat = (event) => {
+  const keyCode = event.keyCode;
+  if (keyCode in beats) {
+    console.log("its owkring");
+    let keyPress = beats[keyCode];
+    keyPress.beat.play();
+  }
+};
 
 /**
  * Keydown listener to fire triggerBeat function
  * HINT: Log the keyCode of the key
  */
 
-document.addEventListener("keydown", (event) => {
-  console.log(event.keyCode);
-});
+document.addEventListener("keydown", triggerBeat);
