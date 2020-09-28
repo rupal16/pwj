fcf13793bd0732256fe36828509e2b91f5f8eacc
Rupal Jain <rupaljain.1699@gmail.com>
2020-09-29 00:08:02 +0530

    initialized color & element values

--- BeatBox/exercises/beatBoxJS/utils.js
+++ BeatBox/exercises/beatBoxJS/utils.js
@@ -24,18 +24,24 @@ class Button {
     this.color = color;
     this.keyCode = keyCode;
     this.element = document.getElementById(keyCode);
-    console.log(this.element);
+    this.setButtonColorInHTML();
+    this.select();
   }
 
   /**
    * Set the button color based on color specified
    */
-  setButtonColorInHTML = () => {};
+  setButtonColorInHTML = () => {
+    this.element.style.backgroundColor = this.color;
+  };
 
   /**
    * Select function to set the background color and boxShadow
    */
-  select = () => {};
+  select = () => {
+    this.element.style.backgroundColor = this.color;
+    this.element.style.boxShadow = `0px 0px 17px 0px ${this.color}`;
+  };
 
   /**
    * Deselect function to reset background color and boxShadow
