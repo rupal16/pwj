b8f62b4ead4d9e66d161a49394294af78fe83c4a
Rupal Jain <rupaljain.1699@gmail.com>
2020-10-04 01:44:25 +0530

    created readData function to get data from data.json

--- BlogAppApi/exercise/api/models/posts.js
+++ BlogAppApi/exercise/api/models/posts.js
@@ -1,9 +1,20 @@
+const PATH = "./data.json";
+const fs = require("fs");
+
 class Post {
-  get() {}
+  get() {
+    return this.readData();
+  }
 
   getIndividualBlog() {}
 
   add() {}
+
+  readData() {
+    let rawData = fs.readFileSync(PATH);
+    let posts = JSON.parse(rawData);
+    return posts;
+  }
 }
 
 module.exports = Post;
