4ea15297917a05ea4934b7e6322dbcf72dfaee98
Rupal Jain <rupaljain.1699@gmail.com>
2020-10-04 22:14:45 +0530

    created storeData function to store data into data.json  together with add() function

--- BlogAppApi/exercise/api/models/posts.js
+++ BlogAppApi/exercise/api/models/posts.js
@@ -8,12 +8,21 @@ class Post {
 
   getIndividualBlog() {}
 
-  add() {}
+  add(newPost) {
+    const currentPosts = this.readData();
+    currentPosts.unshift(newPost);
+    this.storeData(currentPosts);
+  }
 
   readData() {
     let rawData = fs.readFileSync(PATH);
     let posts = JSON.parse(rawData);
     return posts;
+  }
+
+  storeData(rawData) {
+    let data = JSON.stringify(rawData);
+    fs.writeFileSync(PATH, data);
   }
 }
 
