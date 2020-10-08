706b9cd536c34aebffdd458cbe1dd1539c829a06
Rupal Jain <rupaljain.1699@gmail.com>
2020-10-08 20:36:15 +0530

    Created API endpoint for adding a new post without image upload

--- BlogAppApi/exercise/app.js
+++ BlogAppApi/exercise/app.js
@@ -3,6 +3,8 @@ const app = express();
 const Post = require("./api/models/posts");
 const postsData = new Post();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   next();
@@ -27,6 +29,18 @@ app.get("/api/posts/:postId", (req, res) => {
     res.status(404).send("Not found");
     console.log("error");
   }
+});
+
+app.post("/api/posts", (req, res) => {
+  const newPost = {
+    id: `${Date.now()}`,
+    title: req.body.title,
+    content: req.body.content,
+    "post-image": req.body["post-image"],
+    added_date: `${Date.now()}`,
+  };
+  postsData.add(newPost);
+  res.status(201).send(newPost); //201 means something has been created
 });
 
 app.listen(3000, () => console.log("Listening on http://localhost:3000"));
