5ecad9420e43b15b2af336e661ea2b2a38574b90
Rupal Jain <rupaljain.1699@gmail.com>
2020-10-08 23:23:44 +0530

    added the ability to upload file to API

--- BlogAppApi/exercise/app.js
+++ BlogAppApi/exercise/app.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const app = express();
 const Post = require("./api/models/posts");
+var multer = require("multer");
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${file.fieldname}-${Date.now()}${getExt(file.mimetype)}`);
+  },
+});
+
+const getExt = (mimetype) => {
+  switch (mimetype) {
+    case "image/png":
+      return ".png";
+    case "image/jpeg":
+      return ".jpeg";
+  }
+};
+
+var upload = multer({ storage: storage });
 const postsData = new Post();
 
 app.use(express.json());
@@ -31,16 +52,18 @@ app.get("/api/posts/:postId", (req, res) => {
   }
 });
 
-app.post("/api/posts", (req, res) => {
+app.post("/api/posts", upload.single("post-image"), (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
   const newPost = {
     id: `${Date.now()}`,
     title: req.body.title,
     content: req.body.content,
-    "post-image": req.body["post-image"],
+    post_image: req.file.path,
     added_date: `${Date.now()}`,
   };
   postsData.add(newPost);
-  res.status(201).send(newPost); //201 means something has been created
+  res.status(201).send("ok"); //201 means something has been created
 });
 
 app.listen(3000, () => console.log("Listening on http://localhost:3000"));
